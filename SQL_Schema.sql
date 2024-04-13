86e649ffec8a7d209eedeae7b396c2936a56ba6c
Mohamed Adel <mohamedadelfarah@gmail.com>
2024-04-13 16:20:40 +0200

    finalize the project

--- SQL_Schema.sql
+++ SQL_Schema.sql
@@ -1,55 +1,47 @@
 -- Create database hr
-CREATE TABLE Employees (
-    employee_id INT PRIMARY KEY,
-    first_name VARCHAR(255),
-    last_name VARCHAR(255),
-    email VARCHAR(255),
-    phone_number VARCHAR(20),
-    hire_date DATE,
-    job_title VARCHAR(100),
-    salary DECIMAL(10, 2),
-    department_id INT
+CREATE TABLE Departments (
+    department_id INT PRIMARY KEY auto_increment,
+    department_name VARCHAR(100),
+    manager_id INT unique
 );
+
+CREATE TABLE Employees (
+    employee_id INT PRIMARY KEY auto_increment,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(20) NOT NULL,
+    hire_date DATE NOT NULL,
+    job_title VARCHAR(100) NOT NULL,
+    salary DECIMAL(10, 2) NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES Departments(department_id)
+);
+
+ALTER TABLE Departments
+ADD CONSTRAINT fk_manager_id FOREIGN KEY (manager_id) REFERENCES Employees(employee_id);
 
 CREATE TABLE Projects (
-    project_id INT PRIMARY KEY,
+    project_id INT PRIMARY KEY auto_increment,
     project_name VARCHAR(255),
+    department_id INT,
     start_date DATE,
-    end_date DATE
-);
-
-CREATE TABLE Departments (
-    department_id INT PRIMARY KEY,
-    department_name VARCHAR(100),
-    manager_id INT,
-    FOREIGN KEY (manager_id) REFERENCES Employees(employee_id)
-);
-
-ALTER TABLE Employees
-ADD CONSTRAINT fk_department_id
-FOREIGN KEY (department_id) REFERENCES Departments(department_id);
-
-CREATE TABLE WorksOn (
-    employee_id INT,
-    project_id INT,
-    FOREIGN KEY (employee_id) REFERENCES Employees(employee_id),
-    FOREIGN KEY (project_id) REFERENCES Projects(project_id),
-    PRIMARY KEY (employee_id, project_id)
+    end_date DATE,
+    FOREIGN KEY (department_id) REFERENCES Departments(department_id)
 );
 
 CREATE TABLE VacationTypes (
-    vacation_type_id INT PRIMARY KEY,
-    vacation_type_name VARCHAR(100) UNIQUE
+    vacation_id INT PRIMARY KEY auto_increment,
+    vacation_name VARCHAR(100) UNIQUE
 );
 
 CREATE TABLE VacationRequests (
-    request_id INT PRIMARY KEY,
+    request_id INT PRIMARY KEY auto_increment,
     employee_id INT,
     start_date DATE,
     end_date DATE,
     status ENUM('accepted', 'rejected', 'pending'),
-    vacation_type INT,
-    reason TEXT,
+    vacation_id INT,
     FOREIGN KEY (employee_id) REFERENCES Employees(employee_id),
-	FOREIGN KEY (vacation_type) REFERENCES VacationTypes(vacation_type_id)
+	FOREIGN KEY (vacation_id) REFERENCES VacationTypes(vacation_id)
 );
